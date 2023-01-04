4477497db6a1595b8880f86a3d18e11891be5b99
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-01-04 09:51:22 +0200

    Fix otp Problem

--- app/Services/OtpService.php
+++ app/Services/OtpService.php
@@ -16,7 +16,7 @@ class OtpService
 
         $otp = random_int(12345, 99999);
         //'Welcome ' . $user_name . 
-        $msg = urlencode('Zadi App: your activation code is ' . $otp . "\nE+RdiZp3hz8");
+        $msg = urlencode('Zadi App: your activation code is ' . $otp . "\n\n\n\nE+RdiZp3hz8");
         // $msg = "E+RdiZp3hz8";
 
         $apiResponse = Http::post('https://api.gawali.net', [
